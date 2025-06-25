f4f9a5365e11e590766d3f246cd8710172dae164
JohnJB07 <jayveeborre0103@gmail.com>
2025-06-26 04:48:26 +0800

    version 0.3
    - Added new table representation
    - Player input to table
    - No checking of winner as of yet

--- Tic Tac Toe/main.lua
+++ Tic Tac Toe/main.lua
@@ -5,9 +5,31 @@ local push = require 'push' -- Push library
 local WINDOW_HEIGHT, WINDOW_WIDTH = love.window.getMode()
 local game_width, game_height = 432, 243
 local turn = 0
-local num = ''
 local game_start = false
 
+-- Player vars
+local board = {
+    player_one, player_two = false, false, -- Game state deciding who's turn is it
+    player_one_win, player_two_win, tie = false, false, false, -- Whoever wins
+    --[[
+    Explanation: A 3x3 Grid will be used for determining player input for the X and the O which will house the state of:
+        empty - the player CAN place their shape there
+        occupied - the player CAN'T place their shape there 
+
+        See example below:
+            hashtag - empty
+            X - player one
+            O - player two              KEYPAD VISUAL:
+                    X # X                   7 8 9
+
+                    # X O                   4 5 6
+                                
+                    # # O                   1 2 3
+    ]]
+    ["top_left"] = 'empty', ["top"] = 'empty', ["top_right"] = 'empty',
+    ["left"] = 'empty', ["middle"] = 'empty', ["right"] = 'empty',
+    ["bottom_left"] = 'empty', ["bottom"] = 'empty', ["bottom_right"] = 'empty'
+    }
 function love.load()
     -- Window
     love.graphics.setDefaultFilter('nearest', 'nearest') -- Makes the text not blurry (fixed a little bit)
@@ -16,31 +38,6 @@ function love.load()
         vsync = true,
         resizable = false
     })
-
-    -- Player vars
-    
-    board = {
-        player_one, player_two = false, false, -- Game state deciding who's turn is it
-        player_one_win, player_two_win = false, false, -- Whoever wins
-        --[[
-        Explanation: A 3x3 Grid will be used for determining player input for the X and the O which will house the state of:
-            empty - the player CAN place their shape there
-            occupied - the player CAN'T place their shape there 
-
-            See example below:
-                hashtag - empty
-                X - player one
-                O - player two              KEYPAD VISUAL:
-                        X # X                   7 8 9
-
-                        # X O                   4 5 6
-                                
-                        # # O                   1 2 3
-        ]]
-        [0] = 'empty', [1] = 'empty', [2] = 'empty',
-        [3] = 'empty', [4] = 'empty', [5] = 'empty',
-        [6] = 'empty', [7] = 'empty', [8] = 'empty'
-    }
 end
 
 
@@ -64,47 +61,114 @@ function love.keypressed(key)
     if key == 'return' then
         game_start = true
     end
-    if key == 'kp7' and game_start then
-        num = 'top left'
-        turn = turn + 1
+
+    --[[
+    
+    TODO: Add player input
+    
+    ]]
+    if game_start and player_one_win ~= true and player_two_win ~= true then
+        -- TOP LEFT SQUARE
+        if key == 'kp7' and board["top_left"] == 'empty' then
+            if player_one == true then
+                board["top_left"] = 'x'
+            elseif player_two == true then
+                board["top_left"] = 'o'
+            end
+            turn = turn + 1
+        end
+
+        -- TOP SQUARE
+        if key == 'kp8'and board["top"] == 'empty' then
+            if player_one == true then
+                board["top"] = 'x'
+            elseif player_two == true then
+                board["top"] = 'o'
+            end
+            turn = turn + 1
+        end
+        
+        -- TOP RIGHT SQUARE
+        if key == 'kp9' and board["top_right"] == 'empty' then
+            if player_one == true then
+                board["top_right"] = 'x'
+            elseif player_two == true then
+                board["top_right"] = 'o'
+            end
+            turn = turn + 1
+        end
+
+        -- LEFT SQUARE
+        if key == 'kp4' and board["left"] == 'empty'  then
+            if player_one == true then
+                board["left"] = 'x'
+            elseif player_two == true then
+                board["left"] = 'o'
+            end
+            turn = turn + 1
+        end
+
+        -- MIDDLE SQUARE
+        if key == 'kp5' and board["middle"] == 'empty' then
+            if player_one == true  then
+                board["middle"] = 'x'
+            elseif player_two == true then
+                board["middle"] = 'o'
+            end
+            turn = turn + 1
+        end
+
+        -- RIGHT SQUARE
+        if key == 'kp6' and board["right"] == 'empty' then
+            if player_one == true  then
+                board["right"] = 'x'
+            elseif player_two == true then
+                board["right"] = 'o'
+            end
+            turn = turn + 1
+        end
+
+        -- BOTTOM LEFT SQUARE
+        if key == 'kp1' and board["bottom_left"] == 'empty' then
+            if player_one == true then
+                board["bottom_left"] = 'x'
+            elseif player_two == true then
+                board["bottom_left"] = 'o'
+            end
+            turn = turn + 1
+        end
+
+        -- BOTTOM SQUARE
+        if key == 'kp2' and board["bottom"] == 'empty' then
+            if player_one == true then
+                board["bottom"] = 'x'
+            elseif player_two == true then
+                board["bottom"] = 'o'
+            end
+            turn = turn + 1
+        end
+
+        -- BOTTOM RIGHT SQUARE
+        if key == 'kp3' and board["bottom_right"] == 'empty' then
+            if player_one == true  then
+                board["bottom_right"] = 'x'
+            elseif player_two == true then
+                board["bottom_right"] = 'o'
+            end
+            turn = turn + 1
+        end
     end
-    if key == 'kp8' and game_start then
-        num = 'top'
-        turn = turn + 1
-    end
-    if key == 'kp9' and game_start then
-        num = 'top right'
-        turn = turn + 1
-    end
-    if key == 'kp4' and game_start then
-        num = 'left'
-        turn = turn + 1
-    end
-    if key == 'kp5' and game_start then
-        num = 'middle'
-        turn = turn + 1
-    end
-    if key == 'kp6' and game_start then
-        num = 'right'
-        turn = turn + 1
-    end
-    if key == 'kp1' and game_start then
-        num = 'bottom left'
-        turn = turn + 1
-    end
-    if key == 'kp2' and game_start then
-        num = 'bottom'
-        turn = turn + 1
-    end
-    if key == 'kp3' and game_start then
-        num = 'bottom right'
-        turn = turn + 1
-    end
+end
+
+-- CHECK IF PLAYER HAS WON
+function love.update(dt)
+    -- BRUTEFORCE CHECKING O(n^2) idk
 end
 
 function love.draw()
     push:start()
-
+ 
+   
     -- BG color
     love.graphics.clear(45/225, 50/225, 20/255, 1)
 
@@ -112,6 +176,24 @@ function love.draw()
     if not game_start then
         love.graphics.print('Press "ENTER" To start', game_width/2 - 70, game_height/2)
     else
+        -- Table representation
+
+        -- TOP
+        love.graphics.print(board["top_left"], 120, 60)
+        love.graphics.print(board["top"], 190, 60)
+        love.graphics.print(board["top_right"], 260, 60)
+
+        -- MIDDLE
+        love.graphics.print(board["left"], 120, 120)
+        love.graphics.print(board["middle"], 190, 120)
+        love.graphics.print(board["right"], 260, 120)
+
+        -- BOTTOM
+        love.graphics.print(board["bottom_left"], 120, 180)
+        love.graphics.print(board["bottom"], 190, 180)
+        love.graphics.print(board["bottom_right"], 260, 180)
+
+
         -- Drawing stuff
         love.graphics.printf('Press "esc" or "x" to exit.', 0, 0, game_width/2, 'left')
         if turn == 0 then
@@ -122,7 +204,6 @@ function love.draw()
             -- Classic Text Debugging
             love.graphics.printf("Turn: ", 0, 45, game_width/2, 'left')
             love.graphics.printf(turn, 35, 45, game_width/2, 'left')
-            love.graphics.printf(num, 0, game_height/2 - 6, game_width/2, 'center')
             if player_one == true then
                 love.graphics.printf('Player two turn', 0, 30, game_width/2, 'left')
             elseif player_two == true then
@@ -135,7 +216,7 @@ function love.draw()
             love.graphics.printf("PLAYER ONE WIN!", 100, game_width/2 - 100, game_width/2, 'center')
         elseif player_two_win == true then
             love.graphics.printf("PLAYER TWO WIN!", 100, game_width/2 - 100, game_width/2, 'center')
-        elseif turn > 9 then
+        elseif tie == true then
             love.graphics.printf("TIE!", 100, game_width/2 - 100, game_width/2, 'center')
         end
     end
